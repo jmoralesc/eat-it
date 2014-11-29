8bbdb82808013a854d3da0d1c79cb28c3436d20c
Julian Morales Cardenas <julian.morales@crowdint.com>
2014-11-29 13:17:00 -0600

    remove fail migrations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,3 @@
 class Ingredient < ActiveRecord::Base
-  belongs_to :Primary, class_name: 'Recipe'
-  belongs_to :Secondary, class_name: 'Recipe'
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,3 @@
 class Recipe < ActiveRecord::Base
-  has_many :primary_ingredients, foreign_key: :primary, class_name: 'Ingredient'
-  has_many :secondary_ingredients, foreign_key: :secondary, class_name: 'Ingredient'
+
 end

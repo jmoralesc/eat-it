da61330bb49607e087cf0621b8e12fa1df260523
Ana Rosas <a.rosas10@gmail.com>
2014-11-30 13:46:48 -0600

    Change theme and add logo

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141130150041) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "ingredients", force: true do |t|
     t.string   "name"
     t.string   "internal_id"
@@ -33,6 +36,7 @@ ActiveRecord::Schema.define(version: 20141130150041) do
     t.decimal  "score"
     t.decimal  "time"
     t.string   "ingredient"
+    t.integer  "ingredients_array", default: [], array: true
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -52,7 +56,7 @@ ActiveRecord::Schema.define(version: 20141130150041) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
